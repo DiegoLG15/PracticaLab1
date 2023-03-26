5e397c4598d1010fd10ef27e812afa51e0f4610f
DiegoLG15 <a20200839@pucp.edu.pe>
2023-03-26 12:04:50 -0500

    'Practica1'

---
+++ PracticaLab1/src/main/java/com/example/practicalab1/PracticaLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.practicalab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaLab1Application.class, args);
+    }
+
+}
---
+++ PracticaLab1/src/test/java/com/example/practicalab1/PracticaLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicalab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
